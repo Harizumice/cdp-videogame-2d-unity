1587b7308b23e66d44297a3b84ab986e28902cf8
Harimezume <harimezumegalvis2003@gmail.com>
2023-04-27 16:06:48 -0500

    I Agree the Movement of Player in the 360°

--- axol-saiko-unity/Assets/Scripts/objPlayer.cs
+++ axol-saiko-unity/Assets/Scripts/objPlayer.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class objPlayer : MonoBehaviour{
+public class objPlayer : MonoBehaviour {
 
- // Init Variables
-    float xspeed;
+// Init Variables
+   float   xspeed;
+   float   yspeed;
+   Vector3 direction_move;
+   [SerializeField] float spd = 7;
 
-    // Start is called before the first frame update // This is similary of Create Event of Game Maker
-    void Start(){
+// Start is called before the first frame update // This is similary of Create Event of Game Maker
+   void Start(){
+      
+   }
+
+// Update is called once per frame // This is similary of Step Event of Game Maker
+   void Update()
+   {
+      xspeed = Input.GetAxis("Horizontal");
+      yspeed = Input.GetAxis("Vertical");
+
+      direction_move.x   = xspeed;
+      direction_move.y   = yspeed;
         
-    }
-
-    // Update is called once per frame // This is similary of Step Event of Game Maker
-    void Update(){
-        xspeed = input.GetAxis("Horizontal");
-        print(xspeed);
-    }
+      transform.position += direction_move * Time.deltaTime * spd;
+   }
 }
